1dadafbc424623e81aefc75d9d27e58edd245fbd
ori-web <sgh4123@naver.com>
2022-07-05 23:56:39 +0900

    2675번 재채점

--- Backjoon/2675.cpp
+++ Backjoon/2675.cpp
@@ -3,22 +3,23 @@
 #include <string>
 #include <vector>
 
-using namespace std;
-
+//알게 된 사실 string에도 push_back함수를 쓸 수 있다.
 int main() {
 	int count;
-	cin >> count;
-	vector<vector<string>> ps;//문자열을 담기 위한 벡터 지정
-	ps.resize(count);
+	std::cin >> count;
+	std::vector<std::string> ps;//문자열을 담기 위한 벡터 지정
 	for (int i = 0; i < count; i++) {
 		int repeatCount;
-		string s;
-		cin >> repeatCount >> s;
+		std::string s = "";
+		std::string sToP = "";
+		std::cin >> repeatCount >> s;
 		for (auto z : s) {
-			for (int j = 0; j < repeatCount j++)
-				ps[i].push_back(s[j]);
+			for (int i = 0; i < repeatCount; i++) {
+				sToP.push_back(z);
+			}
 		}
+		ps.push_back(sToP);
 	}
-	for (auto i = ps.begin; i != ps.end(); i++)
-		cout << i << endl;
+	for (auto i = ps.begin(); i != ps.end(); i++)
+		std::cout << *i << std::endl;
 }
--- Backjoon/Backjoon.cpp
+++ Backjoon/Backjoon.cpp
@@ -1,1 +1,25 @@
-﻿
+﻿//백준 2675번 문제
+#include <iostream>
+#include <string>
+#include <vector>
+
+
+int main() {
+	int count;
+	std::cin >> count;
+	std::vector<std::string> ps;//문자열을 담기 위한 벡터 지정
+	for (int i = 0; i < count; i++) {
+		int repeatCount;
+		std::string s = "";
+		std::string sToP = "";
+		std::cin >> repeatCount >> s;
+		for (auto z : s) {
+			for (int i = 0; i < repeatCount; i++) {
+				sToP.push_back(z);
+			}
+		}
+		ps.push_back(sToP);
+	}
+	for (auto i = ps.begin(); i != ps.end(); i++)
+		std::cout << *i << std::endl;
+}
