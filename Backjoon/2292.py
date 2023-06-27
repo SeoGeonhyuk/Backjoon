70b179fe6d7c05ea86bb883dd131aa8d447c9d71
sgh4123@naver.com <sgh4123@naver.com>
2023-06-28 08:21:35 +0900

    2292

---
+++ Backjoon/2292.py
@@ -0,0 +1,19 @@
+#include <iostream>
+int main() {
+    int a;
+    int rooms = 6;
+    int cnt = 0;
+    std::cin >> a;
+    a = a - 1;
+    ++cnt;
+    if (a == 0)
+        std::cout << cnt;
+    else {
+        while (a > 0) {
+            a -= rooms;
+            ++cnt;
+            rooms += 6;
+        }
+        std::cout << cnt;
+    }
+}
