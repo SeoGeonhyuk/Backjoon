0ff995c268bb2a8399a8eb4d33e3d454c038c685
Geonhyuk_Seo <60954160+SeoGeonhyuk@users.noreply.github.com>
2023-06-28 18:58:04 +0900

    11279

---
+++ Backjoon/11279.py
@@ -0,0 +1,17 @@
+import heapq
+import sys
+s = []
+cnt = int(input())
+cnt2 = 0
+for i in range(cnt):
+  k = int(sys.stdin.readline())
+  if k > 0:
+    cnt2 += 1
+    heapq.heappush(s, -k)
+  else:
+    if cnt2 > 0:
+      min = -1 * heapq.heappop(s)
+      print(min)
+      cnt2 -= 1
+    else:
+      print(0)
