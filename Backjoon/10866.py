39b39672b5a0bdb4b08a4684a0f0b5416a10ac24
Geonhyuk_Seo <60954160+SeoGeonhyuk@users.noreply.github.com>
2023-06-29 09:11:53 +0900

    10866

---
+++ Backjoon/10866.py
@@ -0,0 +1,39 @@
+import sys
+input = sys.stdin.readline
+s = []
+cnt = int(input())
+for i in range(cnt):
+  order = input().split()
+  if order[0] == "push_back":
+    s.append(order[1])
+  elif order[0] == "push_front":
+    s = [order[1]] + s
+  elif order[0] == "front":
+    if len(s) >= 1:
+      print(s[0])
+    else:
+      print(-1)
+  elif order[0] == "back":
+    if len(s) >= 1:
+      print(s[-1])
+    else:
+      print(-1)
+  elif order[0] == "size":
+    print(len(s))
+  elif order[0] == "empty":
+    if len(s) == 0:
+      print(1)
+    else:
+      print(0)
+  elif order[0] == "pop_front":
+    if len(s) >= 1:
+      print(s[0])
+      s.pop(0)
+    else:
+      print(-1)
+  elif order[0] == "pop_back":
+    if len(s) >= 1:
+      print(s[-1])
+      s.pop(len(s)-1)
+    else:
+      print(-1)
