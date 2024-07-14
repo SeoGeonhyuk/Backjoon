412cf1d83d5dd6ed265e4799fb5aa52c8c10f885
Geonhyuk_Seo <60954160+SeoGeonhyuk@users.noreply.github.com>
2024-07-14 13:54:38 +0900

    [level 2] Title: [3차] 파일명 정렬, Time: 13.46 ms, Memory: 39 MB -BaekjoonHub

--- 프로그래머스/2/17686. ［3차］ 파일명 정렬/［3차］ 파일명 정렬.js
+++ 프로그래머스/2/17686. ［3차］ 파일명 정렬/［3차］ 파일명 정렬.js
@@ -1,6 +1,5 @@
-// 파일 이름을 담을 수 있는 클래스를 제작한다.
 class FileName {
-    constructor(head, number, tail){
+    constructor(head, number, tail) {
         this.head = head;
         this.number = number;
         this.tail = tail;
@@ -8,36 +7,18 @@ class FileName {
 }
 
 function solution(files) {
-    let splitFileArray = files.map((file) => {
-        let head = null;
-        let number = null;
-        let tail = null;
-        for(let i = 0; i < file.length; i++){
-            if(Number.isInteger(parseInt(file[i]))){
-                head = file.substring(0, i);
-                const startNumberPosition = i;
-                for(; Number.isInteger(parseInt(file[i])) !== false; i++){
-                    
-                }
-                number = file.substring(startNumberPosition, i);
-                tail = file.substring(i);
-                break;
-            }
-        }
-        return new FileName(head, number, tail);
+    const splitFileArray = files.map((file) => {
+        const match = file.match(/(\D+)(\d+)(.*)/);
+        return new FileName(match[1], match[2], match[3]);
     });
-    var answer = splitFileArray.sort((a, b) => {
-        if(a.head.toLowerCase() !== b.head.toLowerCase()){
-            return a.head.localeCompare(b.head);
-        }
-        else if(parseInt(a.number) === parseInt(b.number)){
-            return 0;
-        }
-        else{
-            return parseInt(a.number) - parseInt(b.number);
-        }
-    }).map((file) => {
-        return file.head + file.number + file.tail;
+
+    splitFileArray.sort((a, b) => {
+        const headComparison = a.head.toLowerCase().localeCompare(b.head.toLowerCase());
+        if (headComparison !== 0) return headComparison;
+
+        const numberComparison = parseInt(a.number) - parseInt(b.number);
+        return numberComparison;
     });
-    return answer;
+
+    return splitFileArray.map((file) => file.head + file.number + file.tail);
 }
