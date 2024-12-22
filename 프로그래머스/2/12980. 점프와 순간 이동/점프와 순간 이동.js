d96862e9f44b41bf9be01994015ba3940523af8e
SeoGeonhyuk <60954160+SeoGeonhyuk@users.noreply.github.com>
2024-12-22 20:15:44 +0900

    [level 2] Title: 점프와 순간 이동, Time: 0.05 ms, Memory: 33 MB -BaekjoonHub

---
+++ 프로그래머스/2/12980. 점프와 순간 이동/점프와 순간 이동.js
@@ -0,0 +1,12 @@
+function solution(n)
+{
+    var ans = 0;
+    while(n > 0){
+        if(n % 2 === 0) n /= 2;
+        else {
+            ans += 1;
+            n = (n - 1) / 2;
+        }
+    }
+    return ans;
+}
