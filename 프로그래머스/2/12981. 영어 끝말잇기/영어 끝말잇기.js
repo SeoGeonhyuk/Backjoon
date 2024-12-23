067776032c53b6eb1dc4805923c704222371e0d7
SeoGeonhyuk <60954160+SeoGeonhyuk@users.noreply.github.com>
2024-12-23 20:13:24 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.18 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.js
@@ -0,0 +1,19 @@
+function solution(n, words) {
+    var answer = [];
+    const spokeWords = new Map();
+    let beforeWord = words[0][0];
+    words.every((word, index) => {
+        if(!spokeWords.has(word) && word[0] === beforeWord[beforeWord.length - 1]){
+            spokeWords.set(word, true);
+            answer = [ 0, 0 ];
+            beforeWord = word;
+            return true;
+        }
+        else{
+            answer = [index % n + 1, parseInt(index / n + 1)];
+            return false;
+        }
+    })
+
+    return answer;
+}
