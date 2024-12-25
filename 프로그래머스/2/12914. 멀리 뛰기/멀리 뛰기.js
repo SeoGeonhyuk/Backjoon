af95d57eddfc3d7d166ca0465a5827c4ce65e9fd
SeoGeonhyuk <60954160+SeoGeonhyuk@users.noreply.github.com>
2024-12-26 02:41:37 +0900

    [level 2] Title: 멀리 뛰기, Time: 1.30 ms, Memory: 33.9 MB -BaekjoonHub

---
+++ 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.js
@@ -0,0 +1,24 @@
+function solution(n) {
+    const memo = new Map([[0, BigInt(1)], [1, BigInt(1)]]);
+    jumpHyojin(n, memo);
+    return memo.get(n) % BigInt(1234567);
+}
+
+function jumpHyojin(n, memo){
+    if(memo.has(n))
+        return memo.get(n);
+    else{
+        memo.set(n, BigInt(jumpHyojin(n - 1, memo)) + BigInt(jumpHyojin(n - 2, memo)));
+        return memo.get(n);
+    }
+}
+// 재귀를 써야 하는 문제인 거 같다.
+// 하지만 시간 초과가 떴으므로 메모이제이션을 한 번 써보자.
+
+// 4
+// 3 2
+// 2 1 1 0
+
+// Map 객체에 없을 때 있을 때
+// 없을 때 -> 다시 해당 함수 실행
+// 있을 때 찾아서 반환
