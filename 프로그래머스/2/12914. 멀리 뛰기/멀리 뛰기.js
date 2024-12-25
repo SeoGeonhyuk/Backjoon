8f691a663297fd9c657ad31a6e1d4b0ee0f24da9
SeoGeonhyuk <60954160+SeoGeonhyuk@users.noreply.github.com>
2024-12-26 02:52:42 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.57 ms, Memory: 33.7 MB -BaekjoonHub

--- 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.js
+++ 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.js
@@ -1,16 +1,26 @@
+// function solution(n) {
+//     const memo = new Map([[0, BigInt(1)], [1, BigInt(1)]]);
+//     jumpHyojin(n, memo);
+//     return memo.get(n) % BigInt(1234567);
+// }
+
+// function jumpHyojin(n, memo){
+//     if(memo.has(n))
+//         return memo.get(n);
+//     else{
+//         memo.set(n, BigInt(jumpHyojin(n - 1, memo)) + BigInt(jumpHyojin(n - 2, memo)));
+//         return memo.get(n);
+//     }
+// }
+
 function solution(n) {
     const memo = new Map([[0, BigInt(1)], [1, BigInt(1)]]);
-    jumpHyojin(n, memo);
-    return memo.get(n) % BigInt(1234567);
-}
-
-function jumpHyojin(n, memo){
-    if(memo.has(n))
-        return memo.get(n);
-    else{
-        memo.set(n, BigInt(jumpHyojin(n - 1, memo)) + BigInt(jumpHyojin(n - 2, memo)));
-        return memo.get(n);
+    
+    for(let i = 2; i <= n; i++) {
+        memo.set(i, memo.get(i-1) + memo.get(i-2));
     }
+    
+    return memo.get(n) % BigInt(1234567);
 }
 // 재귀를 써야 하는 문제인 거 같다.
 // 하지만 시간 초과가 떴으므로 메모이제이션을 한 번 써보자.
