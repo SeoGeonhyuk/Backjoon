0b915bcc963d78b0f30528e8c33e8c44f9ca2e0d
SeoGeonhyuk <60954160+SeoGeonhyuk@users.noreply.github.com>
2024-12-28 12:37:52 +0900

    [level 2] Title: 괄호 회전하기, Time: 79.41 ms, Memory: 37.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.js
@@ -0,0 +1,26 @@
+function solution(s) {
+    var answer = 0;
+    for(let i = 0; i < s.length; i++){
+        const rotatedSentence = s.slice(i) + s.slice(0, i);
+        answer += checkRightSentence(rotatedSentence);
+    }
+    return answer;
+}
+
+function checkRightSentence(s) {
+    const sentenceStack = [];
+    [...s].forEach((c) => {
+        if(sentenceStack.length && sentenceStack[sentenceStack.length - 1] === '[' && c === ']')
+            sentenceStack.pop();
+        else if(sentenceStack.length && sentenceStack[sentenceStack.length - 1] === '(' && c === ')')
+            sentenceStack.pop();
+        else if(sentenceStack.length && sentenceStack[sentenceStack.length - 1] === '{' && c === '}')
+            sentenceStack.pop();
+        else sentenceStack.push(c);
+    });
+    return sentenceStack.length ? 0 : 1;
+}
+
+
+// 배열을 이용하면 해결될 거 같긴 하다.
+// 배열을 이용해서 해당 문자열이 올바른 괄호를 가지고 있는 문자열인지 체크하는 함수를 만들자.
