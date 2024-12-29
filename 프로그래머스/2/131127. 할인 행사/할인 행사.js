bbc293b52a9ab2b2b6bac63f970b3f5243c6e54a
SeoGeonhyuk <60954160+SeoGeonhyuk@users.noreply.github.com>
2024-12-29 19:54:04 +0900

    [level 2] Title: 할인 행사, Time: 70.50 ms, Memory: 43.1 MB -BaekjoonHub

--- 프로그래머스/2/131127. 할인 행사/할인 행사.js
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.js
@@ -1,34 +1,36 @@
 function solution(want, number, discount) {
-    var answer = 0;
-    const discountMap = new Map();
-    const wantMap = new Map();
-    want.forEach((w, index) => {
-        wantMap.set(w, number[index]);
-    });
-    for(let i = 0; i < discount.length - 9; i++){
-            const saleMap = new Map();
-            let isPossible = true;
-            for(let j = i; j < i + 10; j++){
-                if(j > discount.length - 1){
-                    isPossible = false;
-                    break;
-                }
-                if(!saleMap.has(discount[j])) saleMap.set(discount[j], 0);
-                saleMap.set(discount[j], saleMap.get(discount[j]) + 1);
-            }
-            if(isPossible){
-                const wantMapKeys = wantMap.keys();
-                [...wantMapKeys].every((key) => {
-                    if(saleMap.get(key) === undefined || wantMap.get(key) > saleMap.get(key)){
-                        isPossible = false;
-                        return false;
-                    }
-                    return true;
-                });
-            }
-            if(isPossible) answer += 1;
+  var answer = 0;
+  const wantMap = new Map();
+  want.forEach((w, index) => {
+    wantMap.set(w, number[index]);
+  });
+  for (let i = 0; i < discount.length - 9; i++) {
+    const saleMap = new Map();
+    let isPossible = true;
+    for (let j = i; j < i + 10; j++) {
+      if (j > discount.length - 1) {
+        isPossible = false;
+        break;
+      }
+      if (!saleMap.has(discount[j])) saleMap.set(discount[j], 0);
+      saleMap.set(discount[j], saleMap.get(discount[j]) + 1);
     }
-    return answer;
+    if (isPossible) {
+      const wantMapKeys = wantMap.keys();
+      [...wantMapKeys].every((key) => {
+        if (
+          saleMap.get(key) === undefined ||
+          wantMap.get(key) > saleMap.get(key)
+        ) {
+          isPossible = false;
+          return false;
+        }
+        return true;
+      });
+    }
+    if (isPossible) answer += 1;
+  }
+  return answer;
 }
 
 // 일단, 해당 개수가 있는지 부터 검색해야 한다.
