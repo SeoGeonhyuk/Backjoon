52a92f130e0e2fe74846a04a7a44c4390b1c45fa
SeoGeonhyuk <60954160+SeoGeonhyuk@users.noreply.github.com>
2024-12-29 21:10:51 +0900

    [level 2] Title: H-Index, Time: 0.44 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.js
+++ 프로그래머스/2/42747. H－Index/H－Index.js
@@ -8,6 +8,10 @@ function solution(citations) {
             answer = citation;
         }
     });
+    // 최적화 하는 거 힘들다...
+    // 3ms 나오던 걸 1ms 미만으로 만들어봤다.
+    // 하지만 문제를 풀면서 여러 가지 상황을 잘 고려하지 못했다는 느낌을 받았다. 앞으로는 여러 가지 상황을 선행적으로
+    // 주석을 통해 고려해보는 연습을 해보면 좋을 거 같다.(문제는 하긴 했는데, 잘 안됐던건가...)
     if(answer === 0){
         for(let i = 1; i < sortedCitations[0]; i++){
             if(sortedCitations[0] - i >= 0 && sortedCitations.length >= i) answer = i;
