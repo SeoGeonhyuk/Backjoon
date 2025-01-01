3379ffb1ec2b8a251a154f7b4d85ad6c5ce98513
SeoGeonhyuk <60954160+SeoGeonhyuk@users.noreply.github.com>
2025-01-01 22:10:36 +0900

    [level 2] Title: 튜플, Time: 32.57 ms, Memory: 39.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/64065. 튜플/튜플.js
@@ -0,0 +1,19 @@
+function solution(s) {
+    var answer = [];
+    const numMap = new Map();
+    const sortedS = s.substring(1, s.length - 2).replaceAll('{', '').replaceAll('},', ', ').split(', ').map((k) => k.split(',').map(Number)).sort((a, b) => a.length - b.length);
+    // console.log(sortedS);
+    sortedS.forEach((k) => {
+        k.forEach((num) => {
+            if(!numMap.has(num)) {
+                numMap.set(num, true);
+                answer.push(num);
+            }
+        });
+    })
+    return answer;
+}
+
+// 일단 배열화 시키고
+// 배열의 길이에 따라 오름차 순으로 정렬한다.
+// 새로운 숫자를 기준으로 배열의 원소를 추가하는 방식이면 충분하다.
