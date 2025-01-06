58aa059d08a3faee3ce6895311c4945b22499ba4
SeoGeonhyuk <60954160+SeoGeonhyuk@users.noreply.github.com>
2025-01-07 04:23:43 +0900

    [level 2] Title: 모음 사전, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/84512. 모음 사전/모음 사전.js
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.js
@@ -1,22 +1,24 @@
-function solution(word) {
-    var answer = 0;
-    let startIndex = 0;
-    const alphabetList = ['A', 'E', 'I', 'O', 'U'];
-    const wordList = ['A', 'E', 'I', 'O', 'U'];
-    const wordMap = new Map();
-    while(startIndex <= 5 ** 5) {
-        let word = wordList[startIndex];
-        startIndex += 1;
-        alphabetList.forEach((alphabet) => {
-            if((word + alphabet).length < 6) wordList.push(word + alphabet);
-        });
-    }
-    wordList.sort().some((w, index) => {
-        if(w === word) {
-            answer = index + 1;
-            return true;
-        }
-        return false;
-    });
-    return answer;
+// function solution(word) {
+//     var answer = 0;
+//     let startIndex = 0;
+//     const alphabetList = ['A', 'E', 'I', 'O', 'U'];
+//     const wordList = ['A', 'E', 'I', 'O', 'U'];
+//     while(startIndex <= 5 ** 5) {
+//         let word = wordList[startIndex];
+//         startIndex += 1;
+//         alphabetList.forEach((alphabet) => {
+//             if((word + alphabet).length < 6) wordList.push(word + alphabet);
+//         });
+//     }
+//     wordList.sort().some((w, index) => {
+//         if(w === word) {
+//             answer = index + 1;
+//             return true;
+//         }
+//         return false;
+//     });
+//     return answer;
+// }
+function solution(words) {
+    return words.split('').reduce((r, c, i) => r + [781, 156, 31, 6, 1][i] * ['A', 'E', 'I', 'O', 'U'].indexOf(c) + 1, 0);
 }
