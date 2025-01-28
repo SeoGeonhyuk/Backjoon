37c2c3bb7487346a9eda89a2cffba7bf662d93ca
SeoGeonhyuk <60954160+SeoGeonhyuk@users.noreply.github.com>
2025-01-28 18:45:55 +0900

    [level 2] Title: 2 x n 타일링, Time: 2.57 ms, Memory: 36.9 MB -BaekjoonHub

---
+++ 프로그래머스/2/12900. 2 x n 타일링/2 x n 타일링.js
@@ -0,0 +1,13 @@
+function solution(n) {
+    const MOD = 1000000007;
+    
+    const dp = new Array(n + 1).fill(0);
+    dp[0] = 1;
+    dp[1] = 1;
+    
+    for(let i = 2; i <= n; i++) {
+        dp[i] = (dp[i-1] + dp[i-2]) % MOD;
+    }
+    
+    return dp[n];
+}
