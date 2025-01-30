62abd5f22a97578dc16e779be0977e7f493c09c7
SeoGeonhyuk <60954160+SeoGeonhyuk@users.noreply.github.com>
2025-01-31 08:25:32 +0900

    [level 2] Title: 2개 이하로 다른 비트, Time: 211.21 ms, Memory: 54.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/77885. 2개 이하로 다른 비트/2개 이하로 다른 비트.js
@@ -0,0 +1,46 @@
+// function solution(numbers) {
+//     var answer = [];
+//     numbers.forEach(number => {
+//         const numberBinary = number.toString(2);
+//         const numberBinaryMap = new Map();
+//         for(let i = 0; i < numberBinary.length; i++){
+//             numberBinaryMap.set(numberBinary[i], (numberBinaryMap.get(numberBinary[i]) || 0) + 1);
+//         }
+//         let zeroCount = (numberBinaryMap.get('0') || 0);
+//         let oneCount = (numberBinaryMap.get('1') || 0);
+//         while(Math.abs(zeroCount - (numberBinaryMap.get('0') || 0)) + Math.abs(oneCount - (numberBinaryMap.get('1') || 0)) > 2 || Math.abs(zeroCount - (numberBinaryMap.get('0') || 0)) + Math.abs(oneCount - (numberBinaryMap.get('1') || 0)) === 0) {
+//             number += 1;
+//             zeroCount = 0;
+//             oneCount = 0;
+//             const numberBinary2 = number.toString(2);
+//             for(let i = 0; i < numberBinary2.length; i++){
+//                 if(numberBinary2[i] === '0') zeroCount += 1;
+//                 else oneCount += 1;
+//             }
+//         }
+        
+//         answer.push(number);
+//     })
+//     return answer;
+// }
+
+function solution(numbers) {
+    var answer = [];
+    numbers.forEach((number) => {
+        const numberBinary = number.toString(2);
+        let oneCount = 0;
+        for(let i = numberBinary.length - 1; i >= 0; i--){
+            if(numberBinary[i] === '0') break;
+            else oneCount += 1;
+        }
+        if(oneCount === 0) oneCount = 1;
+        answer.push(number + (2 ** (oneCount - 1)))
+    });
+    return answer;
+}
+//오른쪽부터 0을 만날때까지의 1의 개수를 활용해서 추적할 수 있을 것이다.
+// '10'이라면 11
+// '01이라면 10(2)' + 1
+// '011이라면 100(3) 101(2)' + 2
+// '0111이라면 1000(4) 1001(3) 1010(3) 1011(2)' + 4
+// '01111이라면 10000(5), 10001(4), 10010(4), 10011(3), 10100(4), 10101(3), 10110(3), 10111(2)' +8
